8da632d53118d36126e047deeb9304f392138ee9
amr <adodin@mit.edu>
2021-01-26 17:28:49 -0500

    Corrected Wavefunction Sampling Algorithm

--- PyBloch/sampling.py
+++ PyBloch/sampling.py
@@ -3,32 +3,19 @@
 Written By: Amro Dodin (Willard Group - MIYT)
 """
 
-import numpy as np
 import numpy.random as rnd
 import numpy.linalg as la
 
 
-def sample_magnitudes(num_samples, dim):
-    """ Samples Normalized Magnitudes for Wave-Functions
+def sample_psis(num_samples, dim):
+    """ Samples Wavefunction Uniformly on Hilbert Space
 
     :param num_samples: Number of Wavefunctions to Sample
     :param dim: Hilbert Space Dimension
-    :return: (num_samples, dim) array of wavefunction magnitudes
+    :return: (num_samples, dim) array of wavefunctions
     """
-
-    mags = rnd.uniform(0, 1, (num_samples, dim))
-    mags = mags / la.norm(mags, axis=1, keepdims=True)
-    return mags
-
-
-def sample_phases(num_samples, dim):
-    """ Samples Phase Factors for Wave-Functions Uniformly
-
-    :param num_samples: Number of Wavefunctions to Sample
-    :param dim: Hilbert Space Dimension
-    :return: (num_samples, dim) array of wavefunction magnitudes
-    """
-
-    phis = rnd.uniform(0, 2*np.pi, (num_samples, dim))
-    phases = np.exp(1j*phis)
-    return phases
+    # Sample and Normalize Real & Imag components from Gaussian
+    comps = rnd.standard_normal((num_samples, 2*dim))
+    comps = comps / la.norm(comps, axis=1, keepdims=True)
+    psis = comps[:, :dim] + 1j*comps[:, dim:]
+    return psis
